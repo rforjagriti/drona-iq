9d8889be844d2fa114a5be63d3af5719403736ea
Study Root by Dr R P Singh <rforjagriti@gmail.com>
2026-05-29 08:52:13 +0000

    lkaise add kar saktev hai,firebase account hai droneshwardefenceacademy@

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,13 @@
 
 'use client';
 
+// TODO: Replace placeholders with actual values from your Firebase Console
+// Project Settings -> General -> Your Apps -> Firebase SDK snippet (Config)
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  apiKey: "YOUR_ACTUAL_API_KEY",
+  authDomain: "dronaiq.firebaseapp.com",
+  projectId: "dronaiq",
+  storageBucket: "dronaiq.firebasestorage.app",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
