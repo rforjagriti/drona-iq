ae59857dd1f77b91907e66ab9f0a007f1c927122
Study Root by Dr R P Singh <rforjagriti@gmail.com>
2026-05-29 09:48:13 +0000

    bhai ye hai kya,// Import the functions you need from the SDKs you need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,13 @@
 'use client';
 
 /**
- * Firebase Configuration for Drona IQ
- * 
- * CRITICAL: Replace "AIzaSyB_PASTE_YOUR_ACTUAL_KEY_HERE" with the key
- * found in your Firebase Console Settings: 
- * https://console.firebase.google.com/u/0/project/dronaiq/settings/general
+ * Valid Firebase Configuration for Drona IQ
  */
 export const firebaseConfig = {
-  apiKey: "AIzaSyB_PASTE_YOUR_ACTUAL_KEY_HERE", 
+  apiKey: "AIzaSyCln4rghaCCp-mqXtyL6WS8SIYVpT9YYnM",
   authDomain: "dronaiq.firebaseapp.com",
   projectId: "dronaiq",
   storageBucket: "dronaiq.firebasestorage.app",
-  messagingSenderId: "380315187121", 
+  messagingSenderId: "380315187121",
   appId: "1:380315187121:web:07486f9ab96e12d6f8af90"
 };
