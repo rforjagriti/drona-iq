dfcf61430db0936cbb27e9f4dd0ce26a92b1d4fb
Study Root by Dr R P Singh <rforjagriti@gmail.com>
2026-06-05 09:52:42 +0000

    acha agar mai pic dun to lga doge,ye amit negi ji ki pic hai

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'colab.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
